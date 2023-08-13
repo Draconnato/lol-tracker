1e8da2c1dd0039a7df3564dd0ba2fb6bdfd09451
Draconnato <Gabriel.palma.oliveira@hotmail.com>
2023-08-13 16:12:24 -0300

    Create gitignore file, moved script files to source folder, add requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # token RGAPI-d6d684b0-cfd5-445d-acd1-fcab11b614a1''
 
-from summoner import getSummoner
-from spectator import getCurrentGame
+from source.summoner import getSummoner
+from source.spectator import getCurrentGame
+from credentials import credentials
 
 from pprint import pprint
 
-token = 'RGAPI-d6d684b0-cfd5-445d-acd1-fcab11b614a1'
+token = credentials.TOKEN
 
-summonerData = getSummoner(token,'gambe zambe')
+summonerData = getSummoner(token,'hard')
 
-currentGame = getCurrentGame(token,summonerData['id'])
+currentGame = getCurrentGame(token,summonerData['id']) # the getCurrentGame is limitated by region
 
 participants = currentGame['participants']
+
+print(f"########################################\n\n You are current tracking\n {summonerData['name']} - level {summonerData['summonerLevel']}")
